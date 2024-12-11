feb6606e0a0f032e6a249aa86eabaa0f103a8108
Hunter <hspecial@163.com>
2024-12-11 17:04:14 +0800

    创建应用启动类HunterBlogApplication

---
+++ blog-front/src/main/java/com/hunter/HunterBlogApplication.java
@@ -0,0 +1,18 @@
+package com.hunter;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @author Hunter
+ * @since 2024/12/11
+ *
+ * 博客启动类
+ */
+@SpringBootApplication
+public class HunterBlogApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(HunterBlogApplication.class, args);
+    }
+}
