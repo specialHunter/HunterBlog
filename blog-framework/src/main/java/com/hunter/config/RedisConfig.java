7ee860cd34601ee320ab8b50d3a2ba0c0b44d5c2
Hunter <hspecial@163.com>
2025-02-05 21:27:53 +0800

    【bug修复】Redis使用GenericJackson2JsonRedisSerializer序列化时，无法序列化LocalDateTime类型，需要注册JavaTimeModule。

--- blog-framework/src/main/java/com/hunter/config/RedisConfig.java
+++ blog-framework/src/main/java/com/hunter/config/RedisConfig.java
@@ -1,9 +1,12 @@
 package com.hunter.config;
 
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.datatype.jsr310.JavaTimeModule;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.data.redis.connection.RedisConnectionFactory;
 import org.springframework.data.redis.core.RedisTemplate;
+import org.springframework.data.redis.serializer.GenericJackson2JsonRedisSerializer;
 import org.springframework.data.redis.serializer.RedisSerializer;
 
 /**
@@ -23,10 +26,16 @@ public class RedisConfig {
         // 使用StringRedisSerializer.UTF_8作为key和hash key的序列化器
         template.setKeySerializer(RedisSerializer.string());
         template.setHashKeySerializer(RedisSerializer.string());
+
         // 使用GenericJackson2JsonRedisSerializer作为value和hash value的序列化器。
         // 被序列化的对象，getter方法也会被读取形成property，成为被序列化的一部分
-        template.setValueSerializer(RedisSerializer.json());
-        template.setHashValueSerializer(RedisSerializer.json());
+        ObjectMapper objectMapper = new ObjectMapper();
+        // 注册JavaTimeModule，支持LocalDateTime等时间类型
+        objectMapper.registerModule(new JavaTimeModule());
+        GenericJackson2JsonRedisSerializer jackson2JsonRedisSerializer =
+                new GenericJackson2JsonRedisSerializer(objectMapper);
+        template.setValueSerializer(jackson2JsonRedisSerializer);
+        template.setHashValueSerializer(jackson2JsonRedisSerializer);
 
         // 对配置进行初始化
         template.afterPropertiesSet();
