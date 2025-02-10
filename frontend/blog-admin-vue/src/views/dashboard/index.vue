b964b442618a8d26c0cf5310abd02134df60b829
Hunter <hspecial@163.com>
2025-02-10 10:44:02 +0800

    添加博客后台的前端代码，调整前端代码目录结构

---
+++ frontend/blog-admin-vue/src/views/dashboard/index.vue
@@ -0,0 +1,32 @@
+<template>
+  <div class="dashboard-container">
+    <div class="dashboard-text">
+      博客管理系统
+    </div>
+    <h1 style="margin-left: 36%; margin-top: 16%;">是哪位java卷王让我看看</h1>
+  </div>
+</template>
+
+<script>
+import { mapGetters } from 'vuex'
+export default {
+  name: 'Dashboard',
+  computed: {
+    ...mapGetters([
+      'name'
+    ])
+  }
+}
+</script>
+
+<style lang="scss" scoped>
+.dashboard {
+  &-container {
+    margin: 30px;
+  }
+  &-text {
+    font-size: 30px;
+    line-height: 46px;
+  }
+}
+</style>
