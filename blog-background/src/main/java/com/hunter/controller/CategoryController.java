6f0eb8cf8d426e5fd24b880ad5a5bbecc7aa0914
Hunter <hspecial@163.com>
2025-02-21 23:18:01 +0800

    【特性】获取所有分类列表

--- blog-background/src/main/java/com/hunter/controller/CategoryController.java
+++ blog-background/src/main/java/com/hunter/controller/CategoryController.java
@@ -3,36 +3,30 @@ package com.hunter.controller;
 import com.hunter.domain.ResponseResult;
 import com.hunter.domain.vo.CategoryVo;
 import com.hunter.service.CategoryService;
-import io.swagger.v3.oas.annotations.tags.Tag;
 import jakarta.annotation.Resource;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
 
 /**
- * 分类控制器
- *
  * @author Hunter
- * @since 2025/1/2
+ * @since 2025/2/21
  */
 @RestController
-@RequestMapping("/category")
-@Tag(name = "分类模块", description = "分类相关接口")
+@RequestMapping("/content/category")
 public class CategoryController {
-
     @Resource
     private CategoryService categoryService;
 
     /**
-     * 获取分类列表
+     * 获取所有分类列表
      *
      * @return 分类列表
      */
-    @RequestMapping(value = "/getCategoryList", method = RequestMethod.GET)
-    public ResponseResult<List<CategoryVo>> getCategoryList() {
+    @GetMapping("/listAllCategory")
+    public ResponseResult<List<CategoryVo>> listAllCategory() {
         return categoryService.getCategoryList();
     }
-
 }
