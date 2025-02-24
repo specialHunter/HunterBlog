8fd9dd5277c607e419cdf54cae36c21228e7ceaf
Hunter <hspecial@163.com>
2025-02-24 21:01:08 +0800

    【部署】配置pom.xml，使用maven-assembly-plugin，将项目的依赖包以及自身的代码打包成一个单独的JAR文件，使得项目可以作为独立的单元进行分发和部署

--- blog-front/src/main/java/com/hunter/config/SecurityConfig.java
+++ blog-front/src/main/java/com/hunter/config/SecurityConfig.java
@@ -154,7 +154,7 @@ public class SecurityConfig {
         String message = null;
         if (exception instanceof BadCredentialsException badCredentialsException) {
             message = badCredentialsException.getMessage();
-        } else if (exception instanceof InsufficientAuthenticationException insufficientAuthenticationException) {
+        } else if (exception instanceof InsufficientAuthenticationException) {
             message = "认证失败，请登录后操作";
         } else {
             message = exception.getMessage();
